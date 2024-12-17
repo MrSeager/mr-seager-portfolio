ba0817f43b89af81a32b3d9eb8f591a950c0d0ec
MrSeager <seager1993@gmail.com>
2024-12-17 14:46:59 +0200

    12/17/2024

--- src/components/PortfolioPage.tsx
+++ src/components/PortfolioPage.tsx
@@ -1,69 +1,122 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState, useEffect, useRef } from 'react';
+//Components
+import './PortfolioStyle.css';
+import './DarkTheme.css';
 //Bootsrap
 import 'bootstrap/dist/css/bootstrap.css';
-import { Container, Nav, Navbar, NavbarCollapse, NavDropdown } from 'react-bootstrap';
+import { Container, Nav, Navbar, NavbarCollapse, NavDropdown, Image, Row, Col, Button, Badge } from 'react-bootstrap';
 //Axios
 import axios from 'axios';
 //Spring
 import { useSpring, animated } from '@react-spring/web';
+//Icons
+import { TbExternalLink } from "react-icons/tb";
+import { FaGithub, FaHashtag, FaLinkedin, FaDeviantart, FaYoutube } from "react-icons/fa";
+import { PiDesktopTower, PiDeviceMobile } from "react-icons/pi";
+
+interface PortfolioItemType {
+    id: number;
+    title: string;
+    image: string;
+    technology: string[];
+    version: string[];
+    repasitory: string;
+    link: string;
+}
 
 const PortfolioPage: FC = () => {
     const [theme, setTheme] = useState('dark');
+    const [filters, setFilters] = useState('');
+    const [portfolio, setPortfolio] = useState<PortfolioItemType[]>([]);
 
-    const colors = {
-        dark: [
-            '#262626',
-            '#4D4D4D',
-            '#737373'
-        ],
-        light: [
-            '#F2F2F2',
-            '#D9D9D9',
-            '#A6A6A6'
-        ],
-        blue: [
-            '#011126',
-            '#011C40',
-            '#032859'
-        ]
+    useEffect(() => {
+        axios.get('https://raw.githubusercontent.com/MrSeager/mr-seager-portfolio/refs/heads/main/src/data.json').then((response) => {
+          setPortfolio(response.data.portfolio);
+        });
+    }, []);
+
+    const handleBadgeVerIcon = (ver) => {
+        switch (ver) {
+            case "desktop":
+                return <PiDesktopTower className='h-100 w-100' />;
+            case "mobile":
+                return <PiDeviceMobile className='h-100 w-100' />;
+            default:
+                return null;
+        }
     };
 
-    const props = useSpring({ 
-        from: { 
-            backgroundColor: colors[theme][0] 
-        }, 
-        to: async (next) => { 
-            while (1) { 
-                await next({ 
-                    backgroundColor: colors[theme][1] 
-                }); 
-                await next({ 
-                    backgroundColor: colors[theme][2]  
-                }); 
-            } 
-        },
-        config: { duration: 3000, tension: 100, friction: 20 }
-    });
-
     return (
-        <Container fluid className='min-vh-100 overflow-hidden p-0'>
-            <animated.div style={{ ...props, width: '100%', height: '10vh' }} />
-            <Navbar expand='lg' fixed='top' className='px-5 py-3 bg-transparent'>
-                <Navbar.Brand href='#home' className='text-light'>Mr. Seager's Portfolio</Navbar.Brand>
+        <Container fluid className={`cs-bg-main-${theme} min-vh-100 overflow-hidden px-0 pt-5 pb-0`}>           
+            <Navbar expand='lg' fixed='top' className={`cs-bg-${theme} px-5 py-3 shadow`}>
+                <Navbar.Brand href='#home' className={`fw-bold cs-fc-${theme}`}>Mr. Seager's Portfolio</Navbar.Brand>
+                <Container>
+                    
+                </Container>
                 <Navbar.Toggle />
-                <NavbarCollapse >
+                <NavbarCollapse>
                     <Nav className="ms-auto">
-                        <NavDropdown title="Themes" drop='down'>
+                        <NavDropdown title="Themes" drop='down' className={`cs-nav-link-${theme}`}>
                             <NavDropdown.Item onClick={() => setTheme('dark')}>Dark</NavDropdown.Item>
                             <NavDropdown.Item onClick={() => setTheme('light')}>Light</NavDropdown.Item>
                             <NavDropdown.Item onClick={() => setTheme('blue')}>Blue</NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link className='cs-link text-light'>Contact</Nav.Link>
+                        <Nav.Link className={`cs-link-${theme}`}>Contact</Nav.Link>
                     </Nav>
                 </NavbarCollapse>
             </Navbar>
-            <Container>
-                
+            <Container as={Row} fluid className='mt-5 px-5'>
+                {portfolio.map((portfolioItem, index) => (
+                    <Container as={Col} lg={6} xs={12} key={index} className='px-4 py-2'>                
+                        <Row className={`cs-bg-main-dark${theme} m-0 rounded rounded-3 overflow-hidden shadow pe-2`}>
+                            <Col xs={4} className='px-0 position-relative'>
+                                <Container className='position-absolute d-flex flex-row justify-content-start gap-2 h-25 w-50 mt-2'>
+                                    {portfolioItem.technology != null ? 
+                                        (
+                                            portfolioItem.version.map((ver, index) => (
+                                            <Badge bg="custom" className={`cs-badge-${theme} px-1 py-1`}>
+                                                {handleBadgeVerIcon(ver)}
+                                            </Badge>
+                                            ))
+                                        ) : ''
+                                    }
+                                </Container>
+                                <Image fluid src={`https://raw.githubusercontent.com/MrSeager/mr-seager-portfolio/refs/heads/${portfolioItem.image}`} alt='image' />
+                            </Col>
+                            <Col xs={7} className='d-flex flex-column justify-content-between py-2'>
+                                <h1 className={`cs-fc-${theme}-second h5 m-0`}>{portfolioItem.id}. {portfolioItem.title}</h1>
+                                <Container className='p-0 d-flex flex-row flex-wrap gap-2'>
+                                    {portfolioItem.technology != null ? (
+                                        portfolioItem.technology.map((tech, index) => (
+                                            <Button className={`cs-transition border-0 d-flex flex-row justify-content-center py-0 ps-0 cs-btn-filter-${theme}`}>
+                                                <Badge className='cs-transition cs-badge me-2 p-2 h-100'><FaHashtag className='h-100' /></Badge>
+                                                <p className='fw-bold m-0 pb-1'>{tech}</p>
+                                            </Button>
+                                        ))
+                                    ) : ''}
+                                </Container>
+                            </Col>
+                            <Col xs={1} className='d-flex flex-column align-items-center justify-content-between py-2'>
+                                <Button href={portfolioItem.link} target='_blank' className={`cs-btn-${theme} bg-transparent py-2 cs-transition`}><TbExternalLink /></Button>
+                                <Button href={portfolioItem.repasitory} target='_blank' className={`cs-btn-${theme} bg-transparent py-2 cs-transition`}><FaGithub /></Button>
+                            </Col>
+                        </Row>
+                    </Container>
+                ))}
+            </Container>
+            <Container fluid className={`cs-bg-${theme} mt-3 p-5 shadow d-flex flex-lg-row flex-column`}>
+                <Button className={`cs-transition m-2 cs-btn-${theme} cs-bg-main-${theme}`} href='https://www.linkedin.com/in/sergiy-b-623426159/' target='_blank'>
+                    <FaLinkedin size="1.5em" /> Linkedin
+                </Button>
+                <Button className={`cs-transition m-2 cs-btn-${theme} cs-bg-main-${theme}`} href='https://github.com/MrSeager' target='_blank'>
+                    <FaGithub size="1.5em" /> Github
+                </Button>
+                <Button className={`cs-transition m-2 cs-btn-${theme} cs-bg-main-${theme}`} href='https://www.deviantart.com/mrseager29' target='_blank'>
+                    <FaDeviantart size="1.5em" /> Deviantart
+                </Button>
+                <Button className={`cs-transition m-2 cs-btn-${theme} cs-bg-main-${theme}`} href='https://www.youtube.com/channel/UCQgY4AFsqQWgxOvdeNGs-cQ' target='_blank'>
+                    <FaYoutube size="1.5em" /> YouTube
+                </Button>
             </Container>
         </Container>
     );
