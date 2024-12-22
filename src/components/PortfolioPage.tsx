2ee05c7b4fcb555c304b75739a98ec7a874bff59
MrSeager <seager1993@gmail.com>
2024-12-22 12:00:38 +0200

    12/22/2024

--- src/components/PortfolioPage.tsx
+++ src/components/PortfolioPage.tsx
@@ -9,7 +9,7 @@ import { Container, Nav, Navbar, NavbarCollapse, NavDropdown, Image, Row, Col, B
 //Axios
 import axios from 'axios';
 //Spring
-import { useSpring, animated } from '@react-spring/web';
+import { useSpring, animated, useTrail } from '@react-spring/web';
 //Icons
 import { TbExternalLink } from "react-icons/tb";
 import { FaGithub, FaHashtag, FaLinkedin, FaDeviantart, FaYoutube } from "react-icons/fa";
@@ -27,7 +27,7 @@ interface PortfolioItemType {
 
 const PortfolioPage: FC = () => {
     const [theme, setTheme] = useState('dark');
-    const [filters, setFilters] = useState([]);
+    const [filters, setFilters] = useState<string[]>([]);
     const [portfolio, setPortfolio] = useState<PortfolioItemType[]>([]);
 
     useEffect(() => {
@@ -86,39 +86,41 @@ const PortfolioPage: FC = () => {
                 </NavbarCollapse>
             </Navbar>
             <Container as={Row} fluid className='mt-5 px-5'>
-                {filteredList.map((portfolioItem, index) => (
-                    <Container as={Col} lg={6} xs={12} key={index} className='px-4 py-2'>                
-                        <Row className={`cs-bg-item-${theme} cs-shadow-item-${theme} cs-transition m-0 rounded rounded-3 overflow-hidden pe-2`}>
-                            <Col xs={4} className='px-0 position-relative'>
-                                <Container className='position-absolute d-flex flex-row justify-content-start gap-2 h-25 w-50 mt-2'>
-                                    {portfolioItem.version.map((ver, index) => (
-                                        <Badge key={index} bg="custom" className={`cs-badge-${theme} px-1 py-1 cs-transition`}>
-                                            {handleBadgeVerIcon(ver)}
-                                        </Badge>
-                                    ))}
-                                </Container>
-                                <Image fluid src={`https://raw.githubusercontent.com/MrSeager/mr-seager-portfolio/refs/heads/${portfolioItem.image}`} alt='image' className='cs-img cs-transition' />
-                            </Col>
-                            <Col xs={7} className='d-flex flex-column justify-content-between py-2'>
-                                <h1 className={`cs-fc-${theme}-second h5 m-0`}>{portfolioItem.id}. {portfolioItem.title}</h1>
-                                <Container className='p-0 d-flex flex-row flex-wrap gap-2'>
-                                    {portfolioItem.technology != null ? (
-                                        portfolioItem.technology.map((tech, index) => (
-                                            <Button disabled={filters.includes(tech)} onClick={() => handleAddFilter(tech)} className={`cs-transition border-0 d-flex flex-row justify-content-center py-0 ps-0 cs-btn-filter-${theme}`}>
-                                                <Badge className='cs-transition cs-badge me-2 p-2 h-100'><FaHashtag className='h-100' /></Badge>
-                                                <p className='fw-bold m-0 pb-1'>{tech}</p>
-                                            </Button>
-                                        ))
-                                    ) : ''}
-                                </Container>
-                            </Col>
-                            <Col xs={1} className='d-flex flex-column align-items-center justify-content-between py-2'>
-                                <Button href={portfolioItem.link} target='_blank' className={`cs-btn-${theme} bg-transparent py-2 cs-transition`}><TbExternalLink /></Button>
-                                <Button href={portfolioItem.repasitory} target='_blank' className={`cs-btn-${theme} bg-transparent py-2 cs-transition`}><FaGithub /></Button>
-                            </Col>
-                        </Row>
-                    </Container>
-                ))}
+                {filteredList.length > 0 ? ( 
+                    filteredList.map((portfolioItem, index) => (
+                        <Container as={Col} lg={6} xs={12} key={index} className='px-4 py-2'>                
+                            <Row className={`cs-bg-item-${theme} cs-shadow-item-${theme} cs-transition m-0 rounded rounded-3 overflow-hidden pe-2`}>
+                                <Col xs={4} className='px-0 position-relative'>
+                                    <Container className='position-absolute d-flex flex-row justify-content-start gap-2 h-25 w-50 mt-2'>
+                                        {portfolioItem.version.map((ver, index) => (
+                                            <Badge key={index} bg="custom" className={`cs-badge-${theme} px-1 py-1 cs-transition`}>
+                                                {handleBadgeVerIcon(ver)}
+                                            </Badge>
+                                        ))}
+                                    </Container>
+                                    <Image fluid src={`https://raw.githubusercontent.com/MrSeager/mr-seager-portfolio/refs/heads/${portfolioItem.image}`} alt='image' className='cs-img cs-transition' />
+                                </Col>
+                                <Col xs={7} className='d-flex flex-column justify-content-between py-2'>
+                                    <h1 className={`cs-fc-${theme}-second h5 m-0`}>{portfolioItem.id}. {portfolioItem.title}</h1>
+                                    <Container className='p-0 d-flex flex-row flex-wrap gap-2'>
+                                        {portfolioItem.technology != null ? (
+                                            portfolioItem.technology.map((tech, index) => (
+                                                <Button disabled={filters.includes(tech)} onClick={() => handleAddFilter(tech)} className={`cs-transition border-0 d-flex flex-row justify-content-center py-0 ps-0 cs-btn-filter-${theme}`}>
+                                                    <Badge className='cs-transition cs-badge me-2 p-2 h-100'><FaHashtag className='h-100' /></Badge>
+                                                    <p className='fw-bold m-0 py-1'>{tech}</p>
+                                                </Button>
+                                            ))
+                                        ) : ''}
+                                    </Container>
+                                </Col>
+                                <Col xs={1} className='d-flex flex-column align-items-center justify-content-between py-2'>
+                                    <Button href={portfolioItem.link} target='_blank' className={`cs-btn-${theme} bg-transparent py-2 cs-transition`}><TbExternalLink /></Button>
+                                    <Button href={portfolioItem.repasitory} target='_blank' className={`cs-btn-${theme} bg-transparent py-2 cs-transition`}><FaGithub /></Button>
+                                </Col>
+                            </Row>
+                        </Container>
+                    ))) : <p className='text-center'>Loading...</p>
+                }
             </Container>
             <Container fluid className={`cs-bg-${theme} cs-shadow-${theme} mt-3 p-5 d-flex flex-lg-row flex-column`}>
                 <Button className={`cs-transition m-2 cs-btn-${theme} cs-bg-main-${theme}`} href='https://www.linkedin.com/in/sergiy-b-623426159/' target='_blank'>
