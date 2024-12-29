0247e64ebf263737166d421e3bd9ecf9c88441c3
MrSeager <seager1993@gmail.com>
2024-12-29 10:18:21 +0200

    12/29/2024

--- src/components/ContactPanel.tsx
+++ src/components/ContactPanel.tsx
@@ -13,20 +13,20 @@ interface ContactPanelProps {
 const ContactPanel: FC<ContactPanelProps> = ({ showModal, handleClose, theme }) => {
     return (
         <Modal show={showModal} onHide={handleClose}>
-            <Modal.Header closeButton className={`border-0 rounded-top-1 cs-bg-${theme}`}>
-                <Modal.Title className={`cs-fc-${theme}`}>Contact links</Modal.Title>
+            <Modal.Header className={`border-0 rounded-top-1 cs-bg-${theme}`}>
+                <Modal.Title className={`cs-fc-${theme} text-center mx-auto`}>Contact links</Modal.Title>
             </Modal.Header>
-            <Modal.Body className={`d-flex flex-row cs-bg-main-${theme}`}>
-                <Button className={`cs-transition m-2 cs-btn-${theme} cs-bg-main-${theme}`} href='https://www.linkedin.com/in/sergiy-b-623426159/' target='_blank'>
+            <Modal.Body className={`d-flex flex-row flex-wrap justify-content-center align-items-center gap-3 cs-bg-main-${theme}`}>
+                <Button className={`cs-transition d-flex flex-column justify-content-center align-items-center cs-btn-${theme} cs-bg-main-${theme}`} href='https://www.linkedin.com/in/sergiy-b-623426159/' target='_blank'>
                     <FaLinkedin size="1.5em" /> Linkedin
                 </Button>
-                <Button className={`cs-transition m-2 cs-btn-${theme} cs-bg-main-${theme}`} href='https://github.com/MrSeager' target='_blank'>
+                <Button className={`cs-transition d-flex flex-column justify-content-center align-items-center cs-btn-${theme} cs-bg-main-${theme}`} href='https://github.com/MrSeager' target='_blank'>
                     <FaGithub size="1.5em" /> Github
                 </Button>
-                <Button className={`cs-transition m-2 cs-btn-${theme} cs-bg-main-${theme}`} href='https://www.deviantart.com/mrseager29' target='_blank'>
+                <Button className={`cs-transition d-flex flex-column justify-content-center align-items-center cs-btn-${theme} cs-bg-main-${theme}`} href='https://www.deviantart.com/mrseager29' target='_blank'>
                     <FaDeviantart size="1.5em" /> Deviantart
                 </Button>
-                <Button className={`cs-transition m-2 cs-btn-${theme} cs-bg-main-${theme}`} href='https://www.youtube.com/channel/UCQgY4AFsqQWgxOvdeNGs-cQ' target='_blank'>
+                <Button className={`cs-transition d-flex flex-column justify-content-center align-items-center cs-btn-${theme} cs-bg-main-${theme}`} href='https://www.youtube.com/channel/UCQgY4AFsqQWgxOvdeNGs-cQ' target='_blank'>
                     <FaYoutube size="1.5em" /> YouTube
                 </Button>
             </Modal.Body>
